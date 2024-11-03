b64e796b9573407d92ac3e9f9256e56384c69059
chenziang <3538995003@qq.com>
2024-11-03 20:17:13 +0800

    feat(game): 添加游戏主界面和输入功能
    - 导入 util、tqdm 和 utils.io模块
    - 添加全局变量 total 和 max_choose
    - 实现游戏运行的基本框架，包括欢迎界面、进度条和用户输入
    - 使用 i18n 模块实现国际化支持

--- game.py
+++ game.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 # game.py
-import time, math, random, sys, config, json, argparse,i18n, traceback
+import time, math, random, sys, config, json, argparse, i18n, traceback, util, tqdm, utils.io
 
+_ = i18n.getTranslation
+
+total = 0
+max_choose = 0
 def run():
-    pass
+    global total,max_choose
+    util.print_sep()
+    print(_("meta.game_name"))
+    util.print_sep()
+    for __ in tqdm.trange(100,file=sys.stdout):
+        time.sleep(0.01)
+    total = utils.io.input_number(int, 1, math.inf, _("gui.prompt.input_total_count"))
+    aaa = util.workr(5, utils.io.input_numbers, 5, int, 1, 10, _("gui.prompt.input_total_count"))
+    print(aaa)
