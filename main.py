9fe91ce39090141535871a037012d8259a23d014
chenziang <3538995003@qq.com>
2024-11-08 21:50:58 +0800

    feat(main): 优化游戏崩溃日志输出
    - 引入 Rich 库以美化终端输出
    - 替换 traceback.print_exception 为 console.print_exception
    - 增加额外的错误信息行数和本地变量显示
    - 更新 requirements.txt，添加 Rich 依赖

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 # main.py
 import time, math, random, sys, config, json, argparse,i18n, game, traceback
+from rich.console import Console
+console = Console()
 
 try:
     game.run()
 except Exception as e:
     print("Game crashed!")
     e.add_note("game crashed!")
-    traceback.print_exception(e)
+    # traceback.print_exception(e)
+    console.print_exception(extra_lines=5, show_locals=True, word_wrap=True)
     sys.exit(1)
